00f51f27dff3ccd6bf422a3ab115a8ac1ad572a1
Yanis <yanis.ikene.1@etsmtl.net>
2015-07-30 15:17:33 -0400

    room's messages are deleted when the room is deleted (client-side)

--- client/collections/Messages.js
+++ client/collections/Messages.js
@@ -1,1 +1,8 @@
 Messages = new Meteor.Collection(null);
+
+//Delete room messages when the room is deleted
+Rooms.find().observe({
+	removed: function(room){
+		Messages.remove({"room": room.name});
+	}
+});
